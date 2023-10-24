447a661ea6e02712bd50114836a707346e732bc8
Dev.Huỳnh Thông <dev.huynhvanthong@gmail.com>
2023-10-24 18:49:55 +0700

    no message

--- src/pages/not-authen/index.tsx
+++ src/pages/not-authen/index.tsx
@@ -1,6 +1,8 @@
 import styles from './style.module.scss'
 import {useRouter} from "next/router";
 import {BASE_URL_LOGIN} from "~/utils/Urls";
+import Header from "~/component/Header";
+import React from "react";
 export default function NotAuthentication() {
     const router = useRouter()
 
@@ -9,31 +11,34 @@ export default function NotAuthentication() {
             router.push(BASE_URL_LOGIN)
         }, 300)
     }
-    return <div style={{
-        background: '#022533',
-        width: '100%',
-        height: '100%',
-        position: 'absolute',
-        userSelect: "none"
-    }}>
-        <img
-            style={{
-                height: '60vh',
-                position: 'absolute',
-                left: '50%',
-                top: '30%',
-                transform: 'translate(-50%,-40%)',
-                border: '2px solid #B21065',
-                borderRadius: 30,
-            }}
-            src='/permission.jpg' alt={'Accept Permission'}/>
-        <div >
-            <button
-                onClick={() => handleClickLogin()}
-                className={styles.btnLogin}>
-                Đăng nhập
-            </button>
+    return <>
+        <Header title={"Lopte Link - Not Authentication"}/>
+        <div style={{
+            background: '#022533',
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            userSelect: "none"
+        }}>
+            <img
+                style={{
+                    height: '60vh',
+                    position: 'absolute',
+                    left: '50%',
+                    top: '30%',
+                    transform: 'translate(-50%,-40%)',
+                    border: '2px solid #B21065',
+                    borderRadius: 30,
+                }}
+                src='/permission.jpg' alt={'Accept Permission'}/>
+            <div >
+                <button
+                    onClick={() => handleClickLogin()}
+                    className={styles.btnLogin}>
+                    Đăng nhập
+                </button>
+            </div>
         </div>
-    </div>
+    </>
 }
 NotAuthentication.layout = "client"
--- src/pages/not-support-mobile/index.tsx
+++ src/pages/not-support-mobile/index.tsx
@@ -1,8 +1,9 @@
 import styles from './style.module.scss'
 import {useRouter} from "next/router";
 import {BASE_URL_LOGIN} from "~/utils/Urls";
-import {useEffect} from "react";
+import React, {useEffect} from "react";
 import library from "~/utils/Library";
+import Header from "~/component/Header";
 export default function NotSupportMobile() {
     const router = useRouter()
 
@@ -11,32 +12,35 @@ export default function NotSupportMobile() {
             router.push('admin')
         }
     }, [])
-    return <div style={{
-        background: '#022533',
-        width: '100%',
-        height: '100%',
-        position: 'absolute',
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        justifyItems: "center",
-        alignItems: "center",
-        gap: 10,
-        userSelect: "none",
-        color: "yellow"
-    }}>
-        <img
-            style={{
-                width: 150,
-                borderRadius: 30,
-            }}
-            src='/not-sp-mobile.png' alt={'Accept Permission'}/>
-        <label style={{
-            maxWidth: "80%",
-            fontSize: 25,
-            fontWeight: "bold",
-            textAlign: "center"
-        }}>Trang hiện tại không hỗ trợ phiên bản Điện thoại</label>
-    </div>
+    return <>
+        <Header title={"Lopte Link - Không hỗ trợ mobile"}/>
+        <div style={{
+            background: '#022533',
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            justifyItems: "center",
+            alignItems: "center",
+            gap: 10,
+            userSelect: "none",
+            color: "yellow"
+        }}>
+            <img
+                style={{
+                    width: 150,
+                    borderRadius: 30,
+                }}
+                src='/not-sp-mobile.png' alt={'Accept Permission'}/>
+            <label style={{
+                maxWidth: "80%",
+                fontSize: 25,
+                fontWeight: "bold",
+                textAlign: "center"
+            }}>Trang hiện tại không hỗ trợ phiên bản Điện thoại</label>
+        </div>
+    </>
 }
 NotSupportMobile.layout = "client"
